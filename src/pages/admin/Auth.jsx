922462b92ee750a892b9e032c7c364792a37cb4c
danielmed <daniel.medina@talent.com>
2022-11-10 09:45:18 -0500

    the start of the client for the control asistance

---
+++ src/pages/admin/Auth.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Tab } from "semantic-ui-react";
+//import { RegisterForm } from "../../../components/Admin/Auth";
+import {RegisterForm } from '../../components/Admin/Auth'
+
+
+export function Auth() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const openLogin = () => setActiveIndex(0);
+
+  const panes = [
+    {
+      menuItem: "Entrar",
+      render: () => (
+        <Tab.Pane>
+        <RegisterForm/>
+        </Tab.Pane>
+      ),
+    },
+    {
+      menuItem: "Nuevo usuario",
+      render: () => (
+        <Tab.Pane>
+          <h2>register form</h2>
+        </Tab.Pane>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      
+
+      <Tab
+        panes={panes}
+        
+        activeIndex={activeIndex}
+        onTabChange={(_, data) => setActiveIndex(data.activeIndex)}
+      />
+    </div>
+  );
+}
