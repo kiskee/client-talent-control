8ec0883f16fce6d714d3ba786e9e8aac4ae8cac2
DiamondStalker <cmoreno981@gmail.com>
2022-11-15 18:27:58 -0500

    s

--- src/components/Web/Logout/Logout.jsx
+++ src/components/Web/Logout/Logout.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Icon } from "semantic-ui-react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../hooks";
+import { Auth } from "../../../api";
+import { User } from "../../../api";
+
+const authController = new Auth();
+const userController = new User();
 
 export function Logout() {
   const { logout } = useAuth();
   const navigate = useNavigate();
+
+  const [user, setuser] = useState(second)
+
+  const userName = async () => {
+    try {
+      let accessToken = authController.getAccessToken()
+
+      console.log(accessToken);
+
+      const response = await userController.getMe(accessToken);
+      delete response.password;
+
+      setUser(response);
+      setToken(accessToken);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  userName()
 
   const onLogout = () => {
     logout();
