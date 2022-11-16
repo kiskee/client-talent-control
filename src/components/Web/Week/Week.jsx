68f08cfdd591f286df26c51ee209a4632e07b9b9
DiamondStalker <cmoreno981@gmail.com>
2022-11-16 06:56:21 -0500

    .

--- src/components/Web/Week/Week.jsx
+++ src/components/Web/Week/Week.jsx
@@ -63,6 +63,19 @@ if (cookiesDaysoftheWeek.length == 0) {
     });
 }
 
+// function tarea(){
+//   console.log('acá va la tarea', new Date());
+// }
+
+// function lanzarElDia(momento, tarea){
+//   console.log('lanzado',new Date());
+//   console.log('para ser ejecutado en',momento);
+//   setTimeout(tarea, momento.getTime()-(new Date()).getTime());
+// }
+
+// lanzarElDia(new Date('2016-06-10 20:29'), tarea);
+
+
 const floorSelected = (event) =>{
   console.log(event.target.value)
 }
