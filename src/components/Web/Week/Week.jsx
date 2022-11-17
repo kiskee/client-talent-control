88babb5ed1be7eceeebd942126d0ae5a06493870
DiamondStalker <cmoreno981@gmail.com>
2022-11-16 22:04:55 -0500

    Some change from weeks functionality

--- src/components/Web/Week/Week.jsx
+++ src/components/Web/Week/Week.jsx
@@ -22,52 +22,45 @@ let cookiesDaysoftheWeek = document.cookie
 if (cookiesDaysoftheWeek.length == 0) {
   // The users no has cookies
 
-  //let { cookie, date } = //await validationCookies.getCookieApi();
-
   if (itisFriday()) {
     // The current Date it's Friday; as it is Friday we update the API
-    let days
-    if (new Date(dateAPi) < new Date()) {
-      var dateCalculated = new Date();
-
-      dateCalculated.setDate(dateCalculated.getDate() + 7);
-      dateCalculated.setHours(12, 30, 0);
-
-      days = calculateDays(dateCalculated);
-
-      validationCookies.updateCookieApi({
-        date: dateCalculated,
-        cookie: days.join(","),
-      });
-    }
-
-    document.cookie = "Days of the week" + "=" + days + "; expires=" + dateCalculated + "; path=/";
-  } else {
-    // The current date is not Friday; we create temporary cookies in the API
-
 
 
     validationCookies.getCookieApi()
       .then((response) => {
+        let days
+        if (new Date(response.date) < new Date()) {
+          var dateCalculated = new Date();
+
+          dateCalculated.setDate(dateCalculated.getDate() + 7);
+          dateCalculated.setHours(12, 30, 0);
+
+          days = calculateDays(dateCalculated);
+
+          validationCookies.updateCookieApi({
+            date: dateCalculated,
+            cookie: days.join(","),
+          });
+        }
+
+        document.cookie = "Days of the week" + "=" + days + "; expires=" + dateCalculated.toGMTString() + "; path=/";
+      })
+
+  } else {
+    // The current date is not Friday; we create temporary cookies in the API
+    validationCookies.getCookieApi()
+      .then((response) => {
+        console.log(response.date)
         document.cookie =
-          "Days of the week" + "=" + response.cookie + "; expires=" + response.date + "; path=/";
+          "Days of the week" + "=" + response.cookie + "; expires=" + new Date(response.date).toGMTString() + "; path=/";
       })
   }
-} 
-// else {
-//   cookiesDaysoftheWeek[0]
-//     .split("=")
-//     .pop()
-//     .split(",")
-//     .forEach((temp) => {
-//       day.push(new Date(temp).toLocaleDateString());
-//     });
-// }
+}
 
-const dayys = () =>{
+const dayys = () => {
   let cookiesDaysoftheWeek = document.cookie
-  .split(";")
-  .filter((x) => x.search("Days of the week") > -1);
+    .split(";")
+    .filter((x) => x.search("Days of the week") > -1);
   cookiesDaysoftheWeek[0]
     .split("=")
     .pop()
@@ -78,42 +71,6 @@ const dayys = () =>{
 }
 
 setTimeout(dayys, "1500")
-
-
-
-// if (cookiesDaysoftheWeek.length == 0) {
-//   // As the cookies were not found; a new cookie is created and expires every Friday.
-
-//   var date = new Date();
-
-//   date.setDate(date.getDate() + 7);
-//   date.setHours(12, 30, 0);
-
-//   let days = calculateDays(date);
-//   document.cookie =
-//     "Days of the week" + "=" + days + "; expires=" + date + "; path=/";
-
-// } else {
-//   cookiesDaysoftheWeek[0]
-//     .split("=")
-//     .pop()
-//     .split(",")
-//     .forEach((temp) => {
-//       day.push(new Date(temp).toLocaleDateString());
-//     });
-// }
-
-// function tarea(){
-//   console.log('acá va la tarea', new Date());
-// }
-
-// function lanzarElDia(momento, tarea){
-//   console.log('lanzado',new Date());
-//   console.log('para ser ejecutado en',momento);
-//   setTimeout(tarea, momento.getTime()-(new Date()).getTime());
-// }
-
-// lanzarElDia(new Date('2016-06-10 20:29'), tarea);
 
 const floorSelected = (event) => {
   console.log(event.target.value);
