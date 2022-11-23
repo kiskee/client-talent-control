accecedb06723a6737515df97f46526d23312ab9
Daniel Medina <53805976+kiskee@users.noreply.github.com>
2022-11-22 22:15:25 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { WebRouter, AdminRouter } from "./router";
 import { AuthProvider } from "./contexts";
 
 export default function App() {
   return (
     <AuthProvider>
-      <HashRouter>
+      <BrowserRouter>
         <WebRouter />
         <AdminRouter />
-      </HashRouter>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
